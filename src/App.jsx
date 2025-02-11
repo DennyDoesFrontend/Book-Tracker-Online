539e3e94bc72e3de79fbd0e0280fe34e559dee75
Denzel Ashitey <denzelniiarmahashiteycode@gmail.com>
2025-02-11 11:51:37 +0000

    Card.jsx bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import Card from "./components/Card";
 import Header from "./components/Header";
-
 export default function App() {
   return (
     <main className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] flex flex-col gap-[20px]">
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,8 @@ import Button from "./Button";
 
 export default function Card(props) {
     return (
-        <div className="flex flex-col items-start w-full p-4 shadow-lg max-w-[250px] border-2">
+        <div className="flex flex-col items-start w-full p-4 shadow-lg max-w-[250px] border-2 relative">
+            <img src="/src/assets/edit.svg" alt="Edit" width="35px" className="absolute right-4 top-4 cursor-pointer "/>
             <div>
                 <h5 className="text-2xl text-[#354259] font-semibold">Title</h5>
                 <p className="text-[18px] text-[#354259]">{props.title}</p>
