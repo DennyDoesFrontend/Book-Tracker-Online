4ee1b955d50011f6bc06fa5a5af2638f65f452fb
Denzel Ashitey <denzelniiarmahashiteycode@gmail.com>
2025-02-12 21:24:52 +0000

    layout bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
 import Card from "./components/Card";
 import Header from "./components/Header";
 export default function App() {
+  const books = [
+    {
+      title: 'Harry Potter',
+      pages : 265,
+      author: "Denzel Ashitey"
+    },
+    {
+      title: 'Oliver Twist',
+      pages : 141,
+      author: "Regina Daniels"
+    },
+    {
+      title: 'Street Child',
+      pages : 265,
+      author: "Jim Hawkens"
+    },
+    {
+      title: 'Anonymous Life',
+      pages: 123,
+      author: 'Daniel Brians'
+    },
+  ]
   return (
     <main className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] flex flex-col gap-[20px]">
       <Header />
-      <div>
-        <Card title="Harry Potter" pages="265" author="Denzel Ashitey" />
+      <div className="grid grid-cols-4 gap-3">
+        {
+          books.map( (book)=> (<Card key={book.title} title={book.title} pages={book.pages} author={book.author}/>))
+        }
       </div>
     </main>
   )
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,21 +9,21 @@ export default function Card(props) {
         setIsBrown(!isBrown)
     }
     return (
-        <div className="flex flex-col items-start w-full p-4 shadow-lg max-w-[250px] border-2 relative">
-            <img src="/src/assets/edit.svg" alt="Edit" width="35px" className="absolute right-4 top-4 cursor-pointer "/>
+        <div className="flex flex-col items-start gap-2 w-full p-4 shadow-lg max-w-[250px] border-2 relative">
+            <img src="/src/assets/edit.svg" alt="Edit" width="35px" className="absolute right-4 top-3 cursor-pointer "/>
             <div>
-                <h5 className="text-2xl text-[#354259] font-semibold">Title</h5>
-                <p className="text-[18px] text-[#354259]">{props.title}</p>
+                <h5 className="text-lg text-[#354259]">Title</h5>
+                <p className=" text-[#354259] text-2xl">{props.title}</p>
             </div>
             <div>
-                <h5 className="text-2xl text-[#354259] font-semibold">Pages</h5>
-                <p className="text-[18px] text-[#354259]">{props.pages}</p>
+                <h5 className="text-lg text-[#354259]">Pages</h5>
+                <p className="text-2xl text-[#354259]">{props.pages}</p>
             </div>
             <div>
-                <h5 className="text-2xl text-[#354259] font-semibold">Author</h5>
-                <p className="text-[18px] text-[#354259]">{props.author}</p>
+                <h5 className="text-lg text-[#354259]">Author</h5>
+                <p className="text-2xl text-[#354259]">{props.author}</p>
             </div>
-            <Button onClick={handleClick} text={status} color={isBrown ? '#ECE5C7' : 'red'}/>
+            <Button onClick={handleClick} text={status} color={isBrown ? '#ECE5C7' : 'orange'}/>
             <Button text="Delete" color="#CDC2AE"/>
         </div>
     )
