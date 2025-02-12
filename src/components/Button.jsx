44bb8e004657987aa8a6287d774a34bda2ce1631
Denzel Ashitey <denzelniiarmahashiteycode@gmail.com>
2025-02-12 17:50:20 +0000

    Toggle Finish btn

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
-export default function Button(props) {
+export default function Button({label, color, text, onClick}) {
     return (
         <div className="flex flex-col items-start gap-2 flex-1 w-full">
-            <label className="text-3xl font-medium text-[#354259]">{props.label}</label>
-            <button style={{ backgroundColor: props.color }}  className={`cursor-pointer w-[100%] h-[40px] text-[20px]`} type="submit">{props.text}</button>
+            <label className="text-3xl font-medium text-[#354259]">{label}</label>
+            <button onClick={onClick} style={{ backgroundColor: color }}  className={`cursor-pointer w-[100%] h-[40px] text-[20px]`} type="submit">{text}</button>
         </div>
     )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,13 @@
 import Button from "./Button";
+import { useState } from "react";
 
 export default function Card(props) {
+    const [status, setStatus] = useState("Reading")
+    const [isBrown, setIsBrown] = useState(true)
+    function handleClick() {
+        setStatus(status === 'Reading' ? 'Finished' : 'Reading')
+        setIsBrown(!isBrown)
+    }
     return (
         <div className="flex flex-col items-start w-full p-4 shadow-lg max-w-[250px] border-2 relative">
             <img src="/src/assets/edit.svg" alt="Edit" width="35px" className="absolute right-4 top-4 cursor-pointer "/>
@@ -16,7 +23,7 @@ export default function Card(props) {
                 <h5 className="text-2xl text-[#354259] font-semibold">Author</h5>
                 <p className="text-[18px] text-[#354259]">{props.author}</p>
             </div>
-            <Button text="Read" color="#ECE5C7"/>
+            <Button onClick={handleClick} text={status} color={isBrown ? '#ECE5C7' : 'red'}/>
             <Button text="Delete" color="#CDC2AE"/>
         </div>
     )
